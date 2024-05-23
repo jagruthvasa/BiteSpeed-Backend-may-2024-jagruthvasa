5edf2ee9975138fa5c4007ac6c07bd8dcfad94f0
Anushavasa15 <anushavasa15@gmail.com>
2024-05-23 11:46:22 +0530

    database setup

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,28 @@
 import express, { Request, Response } from "express";
 import bodyParser from "body-parser";
+import mysql, { Connection } from "mysql2/promise";
 
 const app = express();
 const PORT = 3000;
 
 app.use(bodyParser.json());
+
+let connection: Connection;
+
+async function initializeDatabase() {
+	try {
+		connection = await mysql.createConnection({
+			host: "localhost",
+			user: "root",
+			password: "password",
+			database: "fluxkart",
+		});
+		console.log("Database connected");
+	} catch (error) {
+		console.error("Error connecting to the database:", error);
+		process.exit(1);
+	}
+}
 
 app.get("/", (req: Request, res: Response) => {
 	res.send("Server is running");
@@ -12,4 +30,5 @@ app.get("/", (req: Request, res: Response) => {
 
 app.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
+	initializeDatabase();
 });
